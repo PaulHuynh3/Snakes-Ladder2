a09409a94f0d104d1d88b6876256d563ec73319f
Paul <paul.huynh3@gmail.com>
2017-09-09 21:09:08 -0400

    add playerManager

---
+++ Snakes & Ladder/PlayerManager.h
@@ -0,0 +1,13 @@
+//
+//  PlayerManager.h
+//  Snakes & Ladder
+//
+//  Created by Paul on 2017-09-09.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayerManager : NSObject
+
+@end
---
+++ Snakes & Ladder/PlayerManager.m
@@ -0,0 +1,13 @@
+//
+//  PlayerManager.m
+//  Snakes & Ladder
+//
+//  Created by Paul on 2017-09-09.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import "PlayerManager.h"
+
+@implementation PlayerManager
+
+@end
